9e4c495336221086e2f94ec358de6e40865dc54d
HiiroMatsuoka822 <hiiro.bran.gin.378@gmail.com>
2025-01-12 20:45:55 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+let firstName = "Hiiro";
+let lastName = " Matsuoka";
+let yearsOfStudy = "half year for programming";
+let goal = "I aim to combine the business knowledge I gained at my university in Japan with the computer science expertise I am acquiring in the United States, to bridge the gap between the business and engineering fields.";
